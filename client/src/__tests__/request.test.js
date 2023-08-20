b3f26f8e062835fc5b3e950bc3bfba5b2c027b1d
alexanderyoza <alex.yoza@gmail.com>
2023-08-20 16:38:41 -0700

    added tests for request component

---
+++ client/src/__tests__/request.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
+import axios from 'axios';
+import { MemoryRouter } from "react-router-dom";
+import Request from '../Components/Request';
+
+jest.mock('axios');
+
+describe('<Request />', () => {
+    test('renders without crashing', () => {
+
+        render(
+            <MemoryRouter>
+                <Request />
+            </MemoryRouter>
+        );
+    });
+
+    test('alerts the user if required fields are missing', async () => {
+        window.alert = jest.fn();
+
+        render(
+            <MemoryRouter>
+                <Request />
+            </MemoryRouter>
+        );
+        
+        // Click on the submit button
+        fireEvent.click(screen.getByText('Submit'));
+        
+        // Wait for the alert to appear and check its message
+        await waitFor(() => expect(window.alert).toHaveBeenCalledWith('Submit not successful. Please ensure all required boxes are correctly completed.'));
+    });
+
+    test('displays a success message after form submission', async () => {
+        render(
+            <MemoryRouter>
+                <Request />
+            </MemoryRouter>
+        );
+        
+        // Mock the axios response to simulate successful submission
+        axios.post.mockResolvedValueOnce({ data: {} });
+
+        // Assuming you fill in all required fields here...
+        fireEvent.change(screen.getByPlaceholderText('First Name'), { target: { value: 'Test' } });
+        fireEvent.change(screen.getByPlaceholderText('Last Name'), { target: { value: 'Email' } });
+        fireEvent.change(screen.getByPlaceholderText('Email'), { target: { value: 'john.doe@example.com' } });
+        fireEvent.change(screen.getByPlaceholderText('Phone Number'), { target: { value: '1234567890' } });
+
+        // Click on the submit button
+        fireEvent.click(screen.getByText('Submit'));
+
+        // Expect success message to be in the document
+        await screen.findByText('Your request was succesfully submitted');
+    });
+});
